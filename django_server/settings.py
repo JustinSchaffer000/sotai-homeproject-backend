cfafa0cacc29f8a015d01efc25c3e6ed2d5d57b5
JustinSchaffer000 <125526148+JustinSchaffer000@users.noreply.github.com>
2023-02-26 10:48:23 -0700

    Update settings.py

--- django_server/settings.py
+++ django_server/settings.py
@@ -91,18 +91,12 @@ WSGI_APPLICATION = "django_server.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'sotai',  
-        'USER': 'root',  
-        'PASSWORD': '',  
-        'HOST': '127.0.0.1',  
-        'PORT': '3306',  
-        'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        }  
-    }  
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
